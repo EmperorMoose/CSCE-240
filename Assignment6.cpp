901cf4eefa2b16da8d9d62b12cef0a7f671d6588
EmperorMoose <pmatt42@gmail.com>
2016-12-28 22:34:53 -0500

    Update Assignment6.cpp

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -1,137 +1,154 @@
 /***************************************************************************
-* File: Assignment4.cpp
+* File: Assignment6.cpp
 * Author: Pierce Matthews
 * Function: Overloaded string class
 * Input:
-* Output:                                                                        
+* Output:
 ***************************************************************************/
 
 #include "stdafx.h"
 #include "String.h"
 #include <iostream>
 #include <cstdlib>
+#include <stdio.h>
 #include <string>
+#include <string.h>
+#include <cstring>
 
 using namespace std;
 
-class String
+class MyString
 {
-	private:
+private:
 
-		const int max_length = 256;
-		int _length;
-		char data[256];
+	const int max_length = 256;
+	int _length;
+	char data[256];
 
-		//Gets the size of the passed array
-		int getCharArraySize(char arr[])
-		{
-			int len = sizeof(arr) / sizeof(char);
-			return len;
-		}
+	//Gets the size of the passed array
+	int getCharArraySize(char arr[])
+	{
+		int len = strlen(arr) / sizeof(char);
+		return len;
+	}
 
-	public:
+public:
 
-		//default constructor
-		String()
-		{
-			//Assign data to null;
-			data[256] = {'\0'};
-		}
+	//default constructor
+	MyString()
+	{
+		//Assign data to null;
+		data[256] = { '\0' };
+	}
 
-		//destructor
-		~String()
-		{
-			delete data;
-		}
-		//alternate constructor that initializes a string object and sets its char[] to str and update the length variable to be the lenght of str
-		String(char str[])
-		{
-			int len = sizeof(str) / sizeof(char);
-			memcpy(&data, &str, len);
-		}
+	//Copy Constructor
+	MyString(string obj)
+	{
+		string ptr;
+		ptr = obj;
+	}
 
-		//Returns the length of data[]
-		int length()
-		{
-			int len = sizeof(data) / sizeof(char);
-			return len;
-		}
+	//destructor
+	~MyString()
+	{
+		//Wat do
+	}
+	//alternate constructor that initializes a string object and sets its char[] to str and update the length variable to be the lenght of str
+	MyString(char str[])
+	{
+		int len = strlen(str) / sizeof(char);
+		memcpy(&data, &str, len);
+	}
 
-		//Clears data[]
-		void clear()
-		{
-			for (int i = 0; i < sizeof(data); i++) 
-			{
-				data[i] = '\0';
-			}
-		}
+	//Returns the length of data[]
+	int length()
+	{
+		int len = sizeof(data) / sizeof(char);
+		return len;
+	}
 
-		//checks to see if char[] is empty or not
-		bool empty()
+	//Clears data[]
+	void clear()
+	{
+		for (int i = 0; i < sizeof(data); i++)
 		{
-			bool empty = false;
-			if (data[0] == '\0')
-				empty = true;
-			return empty;
+			data[i] = '\0';
 		}
+	}
 
-		//returns the character at the position index
-		char operator[](int index)
-		{
-			char x = data[index];
-			return x;
-		}
+	//checks to see if char[] is empty or not
+	bool empty()
+	{
+		bool empty = false;
+		if (data[0] == '\0')
+			empty = true;
+		return empty;
+	}
 
-		//finds a substring in data
-		int find(char substr[], int startIndex)
-		{
-			int subLen = sizeof(substr) / sizeof(char);
-			int len = sizeof(data) / sizeof(char);
-			for (int i = startIndex; i < len; i++)
-			{
-				if ((data[i] == substr[0]) && (data[i + (subLen - 1)] == substr[subLen]))
-					return i;
-			}
-		}
+	//returns the character at the position index
+	char operator[](int index)
+	{
+		char x = data[index];
+		return x;
+	}
 
-		//Compares two strings and determines if they are equal
-		bool operator==(String &two)
+	//finds a substring in data
+	int find(char substr[], int startIndex)
+	{
+		int subLen = strlen(substr) / sizeof(char);
+		int len = sizeof(data) / sizeof(char);
+		for (int i = startIndex; i < len; i++)
 		{
-			int len = sizeof(data) / sizeof(char);
-			String t = two;
-			for (int i = 0; i < len; i++)
-			{
-				if (t.[i] != data[i])
-					return false;
-			}
-			return true;
+			if ((data[i] == substr[0]) && (data[i + (subLen - 1)] == substr[subLen]))
+				return i;
 		}
+	}
 
-		//concatenates two onto this object
-		bool operator+(String &two)
+	//Compares two strings and determines if they are equal
+	bool operator==(string &two)
+	{
+		int len = sizeof(data) / sizeof(char);
+		string t = two;
+		for (int i = 0; i < len; i++)
 		{
-			int len = sizeof(data) / sizeof(char);
-			String t = two;
-			int strLen = t.length;
-			for (int i = 0; i <= strLen; i++)
-			{
-				data[i] = t[i];
-			}
-			return true;
+			if (t[i] != data[i])
+				return false;
 		}
+		return true;
+	}
 
-		void operator=(String& two)
+	//concatenates two onto this object
+	bool operator+(string &two)
+	{
+		int len = sizeof(data) / sizeof(char);
+		string t = two;
+		int strLen = t.length();
+		for (int i = 0; i <= strLen; i++)
 		{
-			*this = two;
+			data[i] = t[i];
 		}
-		//prints the string to stdout
-		void operator<<(String x)
+		return true;
+	}
+
+	//Assignment operator
+	void operator=(string& two)
+	{
+		*this = two;
+	}
+	//prints the string to stdout
+	void operator<<(string x)
+	{
+		for (int i = 0; i < x.length(); i++)
 		{
-			for (int i = 0; i < x.length; i++)
-			{
-				cout << x[i];
-			}
+			cout << x[i];
 		}
+	}
+
+	//prints the string to stdout
+	void print()
+	{
+		cout << this;
+	}
 };
 
 using namespace std;
@@ -139,7 +156,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	char test[256] = "Hello world";
-	String one(test);
+	MyString one(test);
 	cout << "The value of String one is: ";
 	one.print();
 	char test2[256] = "wor";
@@ -147,3 +164,4 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
