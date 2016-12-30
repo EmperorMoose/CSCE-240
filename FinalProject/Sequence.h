613512bc2cfeafff52951b36169f9b8de675bbc8
EmperorMoose <pmatt42@gmail.com>
2016-12-30 17:41:44 -0500

    Update Sequence.h

--- FinalProject/Sequence.h
+++ FinalProject/Sequence.h
@@ -6,16 +6,19 @@
 #include <string.h>
 #include <iostream>
 
+using namespace std;
+
 class Sequence
 {
 public:
+	friend class Gene;
 	string seq;
 	int len;
-	char seqA[];
+	char seqA[256];
 
 	Sequence();
 	Sequence(string str);
-	Sequence(const Sequence& orig);
+	Sequence(Sequence& orig);
 	virtual ~Sequence();
 
 	void setData(int index, char value);
@@ -24,6 +27,8 @@ public:
 	char at(int index);
 	Sequence & operator=(const char *str);
 	bool isValid(const char *str);
+	void append(string str);
+	void show();
 
 };
 #endif // ! SEQUENCE_H
