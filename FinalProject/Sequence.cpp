a1193a8241c67b592feccf844300b5ed5ba30401
EmperorMoose <pmatt42@gmail.com>
2016-12-29 20:56:57 -0500

    Update Sequence.cpp

--- FinalProject/Sequence.cpp
+++ FinalProject/Sequence.cpp
@@ -1,57 +1,78 @@
-#include "stdafx.h"
-#include <iostream>
-#include <string>
-#include "Gene.h"
-#include "Sequence.h"
-
-class Gene: public Sequence
+class Sequence
 {
-	private:
+private:
+	//Nothing yet
 
-	public:
-		//The minimum overlap
-		int overlap;
-		//The sequence
-		string seq;
-		//The length of the sequence
-		int len;
-		//The Array of the characters in the sequence
-		char seqA[256];
+public:
+	friend class Gene;
+	//The sequence
+	string seq;
+	//The length of the sequence
+	int len;
+	//The Array of the characters in the sequence
+	char seqA[256];
 
-		//The default constructor
-		Gene()
-		{
-			overlap = 1;
-			seq = " ";
-			for (int i = 0; i < len; i++)
-				seqA[i] = '\0';
-		}
+	//Default constuctor sets seq and seqA to be null
+	Sequence()
+	{
+		seq = " ";
+		for (int i = 0; i < len; i++)
+			seqA[i] = '\0';
+	}
 
-		//parameterized constructor
-		Gene(string str, int overlap)
-		{
-			seq = str;
-			overlap = overlap;
-			len = seq.length;
-			for (int i = 0; i < len; i++)
-				seqA[i] = seq.at(i);
-		}
+	//Constructs a sequence based on str[]
+	Sequence(string str)
+	{
+		seq = str;
+		len = str.length;
+		for (int i = 0; i < len; i++)
+			seqA[i] = seq.at(i);
+	}
 
-		Gene(const Gene& orig)
-		{
-			seq = orig.getSeq();
-			len = orig.getLength();
-			for (int i = 0; i < len; i++)
-				seqA[i] = orig.at(i);
-		}
+	//Copy constructor
+	Sequence(Sequence& orig)
+	{
+		seq = orig.getSeq();
+		len = orig.getLength();
+		for (int i = 0; i < len; i++)
+			seqA[i] = orig.at(i);
+	}
 
-		virtual ~Gene()
-		{
-			//This is the deconstructor
-		}
+	virtual ~Sequence()
+	{
+		//This is the destructor
+	}
 
-		int numSeq(ifstream & in_file)
-		{
-			//Assembles the number sequence?
-		}
+	//Sets assigns value to a specific index within seqA
+	void setData(int index, char value)
+	{
+		seqA[index] = value;
+	}
+
+	//Returns the length
+	int getLength()
+	{
+		return len;
+	}
+
+	//Returns the seqs
+	string getSeq()
+	{
+		return seq;
+	}
+
+	//Returns the char at the specified index within seqA
+	char at(int index)
+	{
+		return seqA[index];
+	}
+	Sequence & operator=(const char *str)
+	{
+		//This overloads the = operator in order to assign sequence to the value pointed to by str
+	}
+
+	bool isValid(const char *str)
+	{
+		//Decides if str is valid?
+	}
 };
