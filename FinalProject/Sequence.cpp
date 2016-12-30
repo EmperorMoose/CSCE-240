103c36ab839cfd496417476bd2911a164826cd16
EmperorMoose <pmatt42@gmail.com>
2016-12-30 17:40:57 -0500

    Update Sequence.cpp

--- FinalProject/Sequence.cpp
+++ FinalProject/Sequence.cpp
@@ -1,78 +1,91 @@
-class Sequence
+#include "stdafx.h"
+#include "Sequence.h"
+using namespace std;
+
+//class Sequence
+//{
+//private:
+//Nothing yet
+
+//public:
+//friend class Gene;
+//The sequence
+//string seq;
+//The length of the sequence
+//int len;
+//The Array of the characters in the sequence
+//char seqA[256];
+
+//Default constuctor sets seq and seqA to be null
+Sequence::Sequence()
 {
-private:
-	//Nothing yet
+	//seq = " ";
+	for (int i = 0; i < len; i++)
+		seqA[i] = '\0';
+}
 
-public:
-	friend class Gene;
-	//The sequence
-	string seq;
-	//The length of the sequence
-	int len;
-	//The Array of the characters in the sequence
-	char seqA[256];
+//Constructs a sequence based on str[]
+Sequence::Sequence(string str)
+{
+	seq = str;
+	len = str.length();
+	for (int i = 0; i < len; i++)
+		seqA[i] = seq.at(i);
+}
 
-	//Default constuctor sets seq and seqA to be null
-	Sequence()
-	{
-		seq = " ";
-		for (int i = 0; i < len; i++)
-			seqA[i] = '\0';
-	}
+//Copy constructor
+Sequence::Sequence(Sequence& orig)
+{
+	seq = orig.getSeq();
+	len = orig.getLength();
+	for (int i = 0; i < len; i++)
+		seqA[i] = orig.at(i);
+}
 
-	//Constructs a sequence based on str[]
-	Sequence(string str)
-	{
-		seq = str;
-		len = str.length;
-		for (int i = 0; i < len; i++)
-			seqA[i] = seq.at(i);
-	}
+Sequence::~Sequence()
+{
+	//This is the destructor
+}
 
-	//Copy constructor
-	Sequence(Sequence& orig)
-	{
-		seq = orig.getSeq();
-		len = orig.getLength();
-		for (int i = 0; i < len; i++)
-			seqA[i] = orig.at(i);
-	}
+//Sets assigns value to a specific index within seqA
+void Sequence::setData(int index, char value)
+{
+	seqA[index] = value;
+}
 
-	virtual ~Sequence()
-	{
-		//This is the destructor
-	}
+//Returns the length
+int Sequence::getLength()
+{
+	return len;
+}
 
-	//Sets assigns value to a specific index within seqA
-	void setData(int index, char value)
-	{
-		seqA[index] = value;
-	}
+//Returns the seqs
+string Sequence::getSeq()
+{
+	return seq;
+}
+//Returns the char at the specified index within seqA
+char Sequence::at(int index)
+{
+	return seqA[index];
+}
 
-	//Returns the length
-	int getLength()
-	{
-		return len;
-	}
+void Sequence::append(string str)
+{
+	seq += str;
+}
 
-	//Returns the seqs
-	string getSeq()
-	{
-		return seq;
-	}
+void Sequence::show()
+{
+	cout << seq;
+}
+Sequence & Sequence::operator=(const char *str)
+{
+	//This overloads the = operator in order to assign sequence to the value pointed to by str
+}
 
-	//Returns the char at the specified index within seqA
-	char at(int index)
-	{
-		return seqA[index];
-	}
-	Sequence & operator=(const char *str)
-	{
-		//This overloads the = operator in order to assign sequence to the value pointed to by str
-	}
-
-	bool isValid(const char *str)
-	{
-		//Decides if str is valid?
-	}
-};
+bool Sequence::isValid(const char *str)
+{
+	//Decides if str is valid?
+}
+//};
