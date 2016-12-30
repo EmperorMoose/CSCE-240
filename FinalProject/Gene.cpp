9016bba750becf1ceb429507a3fab3791c9001a0
EmperorMoose <pmatt42@gmail.com>
2016-12-29 20:56:07 -0500

    Update Gene.cpp

--- FinalProject/Gene.cpp
+++ FinalProject/Gene.cpp
@@ -6,52 +6,52 @@
 
 class Gene: public Sequence
 {
-private:
+	private:
 
-public:
-	//The minimum overlap
-	int overlap;
-	//The sequence
-	string seq;
-	//The length of the sequence
-	int len;
-	//The Array of the characters in the sequence
-	char seqA[];
+	public:
+		//The minimum overlap
+		int overlap;
+		//The sequence
+		string seq;
+		//The length of the sequence
+		int len;
+		//The Array of the characters in the sequence
+		char seqA[256];
 
-	//The default constructor
-	Gene()
-	{
-		overlap = 1;
-		seq = " ";
-		for (int i = 0; i < len; i++)
-			seqA[i] = '\0';
-	}
+		//The default constructor
+		Gene()
+		{
+			overlap = 1;
+			seq = " ";
+			for (int i = 0; i < len; i++)
+				seqA[i] = '\0';
+		}
 
-	//parameterized constructor
-	Gene(string str, int overlap)
-	{
-		seq = str;
-		overlap = overlap;
-		len = seq.length;
-		for (int i = 0; i < len; i++)
-			seqA[i] = seq.at(i);
-	}
+		//parameterized constructor
+		Gene(string str, int overlap)
+		{
+			seq = str;
+			overlap = overlap;
+			len = seq.length;
+			for (int i = 0; i < len; i++)
+				seqA[i] = seq.at(i);
+		}
 
-	Gene(const Gene& orig)
-	{
-		seq = orig.getSeq();
-		len = orig.getLength();
-		for (int i = 0; i < len; i++)
-			seqA[i] = orig.at(i);
-	}
+		Gene(const Gene& orig)
+		{
+			seq = orig.getSeq();
+			len = orig.getLength();
+			for (int i = 0; i < len; i++)
+				seqA[i] = orig.at(i);
+		}
 
-	virtual ~Gene()
-	{
-		//This is the deconstructor
-	}
+		virtual ~Gene()
+		{
+			//This is the deconstructor
+		}
 
-	int numSeq(ifstream & in_file)
-	{
-		//Assembles the number sequence?
-	}
+		int numSeq(ifstream & in_file)
+		{
+			//Assembles the number sequence?
+		}
 };
