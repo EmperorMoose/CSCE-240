6a1c7969ca25a43844477650a47aa8ed8d1cf547
EmperorMoose <pmatt42@gmail.com>
2016-12-29 20:55:47 -0500

    Update Gene.h

--- FinalProject/Gene.h
+++ FinalProject/Gene.h
@@ -1,16 +1,16 @@
+#pragma once
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
-/* 
- * File:   Gene.h
- * Author: homayoun
- *
- * Created on November 29, 2016, 4:45 PM
- */
-
+/*
+* File:   Gene.h
+* Author: homayoun
+*
+* Created on November 29, 2016, 4:45 PM
+*/
 #ifndef GENE_H
 #define GENE_H
 #include <iostream>
@@ -22,17 +22,18 @@ using namespace std;
 
 class Gene {
 public:
-    Gene();
-    Gene(ifstream &in_file, ofstream &out_file, int overlap);
-    Gene(const Gene& orig);
-    virtual ~Gene();
-    
-    int numSeq(ifstream & in_file);
+	Gene();
+	Gene(ifstream &in_file, ofstream &out_file, int overlap);
+	Gene(const Gene& orig);
+	virtual ~Gene();
+
+	int numSeq(ifstream & in_file);
 private:
-    Sequence *seq;
-    int size;
+	Sequence *seq;
+	int size;
 
 };
 
 #endif /* GENE_H */
+
 
